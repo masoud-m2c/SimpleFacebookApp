0957b558d2ea9f8b8d08bc08ffd5d986f3fe0c8d
masoud-m2c <masoud@mobiduu.com>
2017-05-10 11:13:02 +0800

    UserRepository created

---
+++ app/UserRepository.php
@@ -0,0 +1,42 @@
+<?php 
+namespace App;
+use Doctrine\ORM\EntityRepository;
+use App\Contract\UserRepositoryInterface;
+
+class UserRepository extends EntityRepository implements UserRepositoryInterface {
+	/**
+	 * @param  array
+	 */
+    public function updateOrCreate(array $user_data=[]) {
+        if (empty($user = $this->findOneBy(array('fb_id' => $user_data['id'])))) {
+			$user = new User;
+		    $user->setFirstName($user_data['first_name']);
+			$user->setLastName($user_data['last_name']);
+			$user->setEmail($user_data['email']);
+			$user->setImage($user_data['picture']['url']);
+			$user->setFbId($user_data['id']);
+			$user->setAccessToken($user_data['access_token']);
+			$user->setStatus($user_data['status']);
+			$user->setCreated(new \DateTime("now"));
+			$this->getEntityManager()->persist($user);
+			$this->getEntityManager()->flush();
+		} else {
+			$this->update($user, $user_data);
+		}
+    }
+
+    /**
+     * @param  User
+     * @param  array
+     */
+    public function update(User $user, array $user_data=[]) {
+    	$user->setFirstName($user_data['first_name']);
+		$user->setLastName($user_data['last_name']);
+		$user->setEmail($user_data['email']);
+		$user->setImage($user_data['picture']['url']);
+		$user->setFbId($user_data['id']);
+		$user->setAccessToken($user_data['access_token']);
+		$user->setStatus($user_data['status']);
+		$this->getEntityManager()->flush();
+    }
+}
---
+++ app/contract/UserRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Contract;
+use App\User;
+
+interface UserRepositoryInterface {
+    public function updateOrCreate(array $user_data = []);
+    public function update(User $user, array $user_data = []);
+}
